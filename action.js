d3ef8a3e21043a6b0bf09c3cab78190b248c40e7
Raman Gupta <rocketraman@gmail.com>
2020-01-30 03:33:29 -0500

    Implement from: pullrequest, string

--- action.js
+++ action.js
@@ -6,6 +6,7 @@ const issueIdRegEx = /([a-zA-Z0-9]+-[0-9]+)/g
 const eventTemplates = {
   branch: '{{event.ref}}',
   commits: "{{event.commits.map(c=>c.message).join(' ')}}",
+  pullrequest: "{{event.pull_request.head.ref}}",
 }
 
 module.exports = class {
